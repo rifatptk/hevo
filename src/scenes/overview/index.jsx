2a3862d1f06f4debd78923c3cd504d064a64017a
rifatptk <rifatptk@gmail.com>
2022-11-29 19:46:40 +0600

    added some things

--- src/scenes/overview/index.jsx
+++ src/scenes/overview/index.jsx
@@ -1,7 +1,44 @@
+import { Box, Tooltip, Typography, useTheme } from '@mui/material';
 import React from 'react';
+import { tokens } from '../../theme';
+import NorthIcon from '@mui/icons-material/North';
 
 const Overview = () => {
-  return <div>Overview</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box px="20px">
+      <Box
+        p="20px"
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          backgroundColor: colors.primary[400],
+          borderRadius: '5px',
+        }}
+      >
+        <Tooltip title="Click to assign a higher priority" place="right">
+          <NorthIcon
+            color={colors.primary[400]}
+            sx={{
+              p: '4px',
+              backgroundColor: colors.primary[300],
+              borderRadius: '4px',
+            }}
+          />
+        </Tooltip>
+        <Typography>#1</Typography>
+        <Box
+          sx={{
+            height: '20px',
+            width: '1px',
+            backgroundColor: colors.primary[300],
+          }}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Overview;
